7907cadd03e21ffc2f3570bd729e8870aabeafe8
revolt3245 <44830134+revolt3245@users.noreply.github.com>
2018-11-16 02:09:09 +0900

    Add files via upload

--- brainfuck computer/loop_stack.v
+++ brainfuck computer/loop_stack.v
@@ -11,13 +11,14 @@ always @(*)
 begin
   if(push & !pop)
   begin
-    stack[stack_pointer] = address_in;
-    stack_pointer = stack_pointer + 5'b0_0001;
+    stack[stack_pointer[4:0]] = address_in;
+    stack_pointer[4:0] = stack_pointer[4:0] + 5'b0_0001;
   end
   else if(!push & pop)
   begin
-    stack_pointer = stack_pointer - 5'b0_0001;
-    address_out = stack[stack_pointer];
+    stack[stack_pointer[4:0]][9:0] = 10'b00_0000_0000;
+    stack_pointer[4:0] = stack_pointer[4:0] - 5'b0_0001;
+    address_out = stack[stack_pointer[4:0]];
   end
 end
 endmodule
